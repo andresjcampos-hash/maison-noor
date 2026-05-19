ba3835d90bc13879d8c364360bd02781d6dafafa
Andre Batista (Serveng Mineracao - Pedreira Jambeiro) <22602870854@serveng.grupo>
2026-05-19 15:50:55 -0300

    Corrige sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://www.maisonnoor.com.br";
 
   return [
@@ -9,27 +9,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
-    },
-
-    {
-      url: `${baseUrl}/novidades`,
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
-    },
-
-    {
-      url: `${baseUrl}/checkout`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-
-    {
-      url: `${baseUrl}/crm`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.2,
     },
   ];
 }
